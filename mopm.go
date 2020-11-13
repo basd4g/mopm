09d74659f7354ef37b2970b4f865f1388a1f517e
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-14 01:18:42 +0900

    Add subcommand 'lint'

--- mopm.go
+++ mopm.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/go-yaml/yaml"
 	"github.com/urfave/cli"
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 )
 
 type Package struct {
@@ -49,6 +51,20 @@ func main() {
 
 					printPackage(pkg)
 					return nil
+				},
+			},
+			{
+				Name:  "lint",
+				Usage: "check package definition file format",
+				Action: func(c *cli.Context) error {
+					packagePath := c.Args().First()
+					pkg, err := readPackageFile(packagePath)
+					if err != nil {
+						log.Fatal(err)
+						return err
+					}
+					err = checkPackageFormat(pkg)
+					return err
 				},
 			},
 		},
@@ -95,4 +111,44 @@ func printPackage(pkg *Package) {
 		fmt.Print("(" + env.Architecture + ", " + env.Platform + ")")
 	}
 	fmt.Println()
+}
+
+func checkPackageFormat(pkg *Package) error {
+	pkgNameRegex := regexp.MustCompile(`^[0-9a-z\-]+$`)
+	if !pkgNameRegex.MatchString(pkg.Name) {
+		return errors.New("package name must consist of a-z, 0-9 and -(hyphen) charactors")
+	}
+	urlRegex := regexp.MustCompile(`^https?://`)
+	if !urlRegex.MatchString(pkg.Url) {
+		return errors.New("package url must start with http(s):// ... ")
+	}
+	if pkg.Description == "" {
+		return errors.New("package url must not be empty")
+	}
+	if len(pkg.Environments) == 0 {
+		return errors.New("package environment must not be empty")
+	}
+	for _, env := range pkg.Environments {
+		if env.Architecture != "x86_64" {
+			return errors.New("package environment architecture must be 'x86_64'")
+		}
+		if env.Platform != "darwin" && env.Platform != "ubuntu" {
+			return errors.New("package environment architecture must be 'darwin' || 'ubuntu'")
+		}
+		for _, dpkg := range env.Dependencies {
+			if !pkgNameRegex.MatchString(dpkg) {
+				return errors.New("package environment dependencies package name must consist of a-z, 0-9 and -(hyphen) charactors")
+			}
+		}
+		if env.Verification == "" {
+			return errors.New("package environment verification must not be empty")
+		}
+		if env.Privilege != "root" && env.Privilege != "unnecessary" && env.Privilege != "never" {
+			return errors.New("package environment architecture must be 'root', 'unnecessary' or 'never'")
+		}
+		if env.Script == "" {
+			return errors.New("package environment script must not be empty")
+		}
+	}
+	return nil
 }
