59f4f34ac3bfe8826979f9abb4eddbeab992b6c9
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-13 22:20:02 +0900

    Read yaml

--- mopm.go
+++ mopm.go
@@ -4,8 +4,24 @@ import (
   "os"
   "fmt"
   "log"
+  "io/ioutil"
   "github.com/urfave/cli"
+  "github.com/go-yaml/yaml"
 )
+
+type Package struct {
+  Name string
+  Url string
+  Description string
+  Environments []struct {
+    Architecture string
+    Platform string
+    Dependencies []string
+    Verification string
+    Privilege string
+    Script string
+  }
+}
 
 func main() {
   app := &cli.App {
@@ -17,7 +33,14 @@ func main() {
         Name: "search",
         Usage: "search package",
         Action: func (c *cli.Context) error {
-          fmt.Printf("%s\n", c.Args().First())
+          packageName := c.Args().First()
+          packagePath := "definitions/" + packageName + ".mopm.yaml"
+          _, error := os.Stat(packagePath)
+          if error != nil {
+            log.Fatal("Error: The package did not exists")
+            return error
+          }
+          readFile(packagePath)
           return nil
         },
       },
@@ -28,4 +51,29 @@ func main() {
   if err !=nil {
     log.Fatal(err)
   }
+}
+
+func readFile (path string) {
+  buf, err := ioutil.ReadFile(path)
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+
+  data, err := ReadOnStruct(buf)
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+  fmt.Println(*data)
+}
+
+func ReadOnStruct(fileBuffer []byte) (*Package, error) {
+  p := Package{}
+  err := yaml.Unmarshal(fileBuffer, &p)
+  if err != nil {
+    fmt.Println(err)
+    return nil, err
+  }
+  return &p, nil
 }
