5a2abe44b23b83c69d4ea4382a89fae4f185fd7c
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-13 18:31:54 +0900

    Update hello, world

--- mopm.go
+++ mopm.go
@@ -3,19 +3,23 @@ package main
 import (
   "os"
   "fmt"
+  "log"
   "github.com/urfave/cli"
 )
 
 func main() {
-  app := cli.NewApp()
-
-  app.Name = "mopm"
-  app.Usage = "Mopm (Manager Of Package Maganger) is meta package manager for cross platform environment."
-  app.Version = "0.0.1"
-  app.Action = func  (context *cli.Context) error {
-    fmt.Println("hello, world!")
-    return nil
+  app := &cli.App {
+    Name: "mopm",
+    Usage: "Mopm (Manager Of Package Maganger) is meta package manager for cross platform environment.",
+    Version: "0.0.1",
+    Action: func  (context *cli.Context) error {
+      fmt.Println("hello, world!")
+      return nil
+    },
   }
 
-  app.Run(os.Args)
+  err := app.Run(os.Args)
+  if err !=nil {
+    log.Fatal(err)
+  }
 }
