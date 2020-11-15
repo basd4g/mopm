63a97a39cd7fa3491e4ea0cb940614e3a8ed8018
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-15 16:41:08 +0900

    Add test

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 all:
 	go build mopm.go
+test:
+	go test
 clean:
 	rm -rf mopm
-.PHONY: clean
+.PHONY: test clean
 
---
+++ mopm_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMachinePlatform(t *testing.T) {
+	got := machinePlatform()
+	if got != "linux/ubuntu" {
+		t.Errorf("machinePlatform() = %s, want linux/ubuntu", got)
+	}
+}
+
+func TestLintPackage(t *testing.T) {
+	pkg := Package{
+		Name:        "package-name",
+		Url:         "https://example.com",
+		Description: "This sentence is package description!",
+		Environments: []Environment{
+			{
+				Architecture: "amd64",
+				Platform:     "linux/ubuntu",
+				Dependencies: []string{},
+				Verification: "verificationCommand",
+				Privilege:    false,
+				Script:       "installComannds",
+			},
+		},
+	}
+
+	// success
+	got := lintPackage(&pkg)
+	if got != nil {
+		t.Errorf("lintPackage(&pkg) = %s, want nil", got)
+	}
+
+	// fail: name
+	pkg.Name = "with space"
+	got = lintPackage(&pkg)
+	expected := "Package name must consist of a-z, 0-9 and -(hyphen) charactors"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Name = "package-name"
+
+	// fail: url
+	pkg.Url = "ftp://example.com/hoge/fuga"
+	got = lintPackage(&pkg)
+	expected = "Package url must start with http(s):// ... "
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Url = "https://example.com"
+
+	// fail: description
+	pkg.Description = ""
+	got = lintPackage(&pkg)
+	expected = "Package description must not be empty"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Description = "This sentence is package description!"
+
+	// fail: architecture
+	pkg.Environments[0].Architecture = "unknown"
+	got = lintPackage(&pkg)
+	expected = "Package architecture must be 'amd64'"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Environments[0].Architecture = "amd64"
+
+	// fail: platform
+	pkg.Environments[0].Platform = "unknown"
+	got = lintPackage(&pkg)
+	expected = "Package architecture must be 'darwin' or 'linux/ubuntu'"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Environments[0].Platform = "linux/ubuntu"
+
+	// fail: dependencies
+	pkg.Environments[0].Dependencies = []string{"valid-package-name", "unvalid package name"}
+	got = lintPackage(&pkg)
+	expected = "Package dependencies must consist of a-z, 0-9 and -(hyphen) charactors"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Environments[0].Dependencies = []string{}
+
+	// fail: verification
+	pkg.Environments[0].Verification = ""
+	got = lintPackage(&pkg)
+	expected = "Package verification must not be empty"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Environments[0].Verification = "verificationCommand"
+
+	// fail: verification
+	pkg.Environments[0].Script = ""
+	got = lintPackage(&pkg)
+	expected = "Package script must not be empty"
+	if got != nil && got.Error() != expected {
+		t.Errorf("lintPackage(&pkg) = '%s', want '%s'", got, expected)
+	}
+	pkg.Environments[0].Script = "installCommands"
+}
+
+/*
+TODO: Write tests for the following functions...
+
+func main() {
+func search(packageName string) error {
+func lint(packagePath string) error {
+func verify(packageName string) error {
+func install(packageName string) error {
+func readPackageFile(path string) (*Package, error) {
+func printPackage(pkg *Package) {
+func verifyPackage(pkg *Package) error {
+func installPackage(pkg *Package) error {
+func environmentOfTheMachine(pkg *Package) (*Environment, error) {
+func machineEnvId() string {
+func execBash(script string) error {
+func execBashUnsudo(script string) error {
+func message(s string) {
+*/
