a46d4f535d17c12602de563bfafebf61120778af
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-15 18:36:41 +0900

    Add test of readPackageFile()

--- mopm_test.go
+++ mopm_test.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestMachinePlatform(t *testing.T) {
 	got := machinePlatform()
 	if got != "linux/ubuntu" {
-		t.Errorf("machinePlatform() = %s, want linux/ubuntu", got)
+		t.Errorf("machinePlatform() = %s, want 'linux/ubuntu'", got)
 	}
 }
 
 func TestMachineEnvId(t *testing.T) {
 	got := machineEnvId()
 	if got != "amd64@linux/ubuntu" {
-		t.Errorf("machineEnvId() = %s, want amd64@linux/ubuntu", got)
+		t.Errorf("machineEnvId() = %s, want 'amd64@linux/ubuntu'", got)
 	}
 }
 
@@ -114,6 +115,59 @@ func TestLintPackage(t *testing.T) {
 	pkg.Environments[0].Script = "installCommands"
 }
 
+func TestReadPackageFile(t *testing.T) {
+	dir, _ := os.Getwd()
+	pkg, err := readPackageFile(dir + "/sample.mopm.yaml")
+	if err != nil {
+		t.Errorf("readPackageFile() return error: %s, want nil", err)
+	}
+	if pkg.Name != "sample" {
+		t.Errorf("readPackageFile() return pkg.Name = %s, want 'sample'", pkg.Name)
+	}
+	if pkg.Url != "https://github.com/basd4g/mopm" {
+		t.Errorf("readPackageFile() return pkg.Url = %s, want 'https://github.com/basd4g/mopm'", pkg.Url)
+	}
+	if pkg.Description != "This is sample package definition script. It cannot be installed." {
+		t.Errorf("readPackageFile() return pkg.Description = %s, want 'This is sample package definition script. It cannot be installed.'", pkg.Description)
+	}
+	if pkg.Environments[0].Architecture != "amd64" {
+		t.Errorf("readPackageFile() return pkg.Environments[0].Architecture = %s, wants 'amd64'", pkg.Environments[0].Architecture)
+	}
+	if pkg.Environments[0].Platform != "darwin" {
+		t.Errorf("readPackageFile() return pkg.Environments[0].Platform = %s, wants 'darwin'", pkg.Environments[0].Platform)
+	}
+	if pkg.Environments[0].Dependencies != nil {
+		t.Errorf("readPackageFile() return pkg.Environments[0].Dependencies = %s, nil", pkg.Environments[0].Dependencies)
+	}
+	if pkg.Environments[0].Verification != "false && false" {
+		t.Errorf("readPackageFile() return pkg.Environments[0].Verification = %s, wants 'false && false'", pkg.Environments[0].Verification)
+	}
+	if pkg.Environments[0].Privilege != false {
+		t.Errorf("readPackageFile() return pkg.Environments[0].Privilege = true, wants false")
+	}
+	if pkg.Environments[0].Script != "echo \"This is sample install script. It is no excution anyware.\"\n" {
+		t.Errorf("readPackageFile() return pkg.Environments[0].Script = %s, wants echo \"This is sample install script. It is no excution anyware.\"\n", pkg.Environments[0].Script)
+	}
+	if pkg.Environments[1].Architecture != "amd64" {
+		t.Errorf("readPackageFile() return pkg.Environments[1].Architecture = %s, wants 'amd64'", pkg.Environments[1].Architecture)
+	}
+	if pkg.Environments[1].Platform != "linux/ubuntu" {
+		t.Errorf("readPackageFile() return pkg.Environments[1].Platform = %s, wants 'linux/ubuntu'", pkg.Environments[1].Platform)
+	}
+	if pkg.Environments[1].Dependencies != nil {
+		t.Errorf("readPackageFile() return pkg.Environments[1].Dependencies = %s, nil", pkg.Environments[1].Dependencies)
+	}
+	if pkg.Environments[1].Verification != "false && false" {
+		t.Errorf("readPackageFile() return pkg.Environments[1].Verification = %s, wants 'false && false'", pkg.Environments[1].Verification)
+	}
+	if pkg.Environments[1].Privilege != true {
+		t.Errorf("readPackageFile() return pkg.Environments[1].Privilege = false, wants true")
+	}
+	if pkg.Environments[1].Script != "echo \"This is sample install script. It is no excution anyware.\"\n" {
+		t.Errorf("readPackageFile() return pkg.Environments[1].Script = %s, wants echo \"This is sample install script. It is no excution anyware.\"\n", pkg.Environments[1].Script)
+	}
+}
+
 /*
 TODO: Write tests for the following functions...
 
@@ -122,7 +176,6 @@ func search(packageName string) error {
 func lint(packagePath string) error {
 func verify(packageName string) error {
 func install(packageName string) error {
-func readPackageFile(path string) (*Package, error) {
 func printPackage(pkg *Package) {
 func verifyPackage(pkg *Package) error {
 func installPackage(pkg *Package) error {
