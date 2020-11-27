949f96c5dcf332d8b96d9e1b703977eb9d06bbbb
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-27 19:14:05 +0900

    Update install.sh installing to /usr/local/bin/mopm

--- install.sh
+++ install.sh
@@ -6,16 +6,23 @@ if [ "$(whoami)" != "root" ]; then
   exit 1
 fi
 
-if [ "$(uname)" == "Linux" ]; then
+
+if [ "$(uname)" = "Linux" ]; then
   target="linux"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [ "$(uname)" = "Darwin" ]; then
   target="darwin"
 else
   echo "Unsupport Operating System... only linux or macOS"
   exit 1
 fi
 
-curl -sL "https://github.com/basd4g/mopm/releases/download/0.0.2/mopm-amd64-${target}.out" > /usr/bin/mopm
-chmod a+x /usr/bin/mopm
+install_path="/usr/local/bin/mopm"
 
-echo "mopm is installed!"
+curl -sL "https://github.com/basd4g/mopm/releases/download/0.0.2/mopm-amd64-${target}.out" > "${install_path}"
+chmod a+x "${install_path}"
+
+if which mopm; then
+  echo "Mopm is installed!  If you want to uninstall mopm, please delete '${install_path}.'"
+else
+  "Installing mopm is failed... The script tried to install mopm to '${install_path}', but mopm is not found with 'which mopm'."
+fi
