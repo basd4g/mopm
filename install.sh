c7fff46a4061621da1be18eff3831f61a29883ab
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-27 18:54:28 +0900

    Support macOS on install script

--- install.sh
+++ install.sh
@@ -6,7 +6,16 @@ if [ "$(whoami)" != "root" ]; then
   exit 1
 fi
 
-curl -sL https://github.com/basd4g/mopm/releases/download/0.0.2/mopm-amd64-linux.out > /usr/bin/mopm
+if [ "$(uname)" == "Linux" ]; then
+  target="linux"
+elif [ "$(uname)" == "Darwin" ]; then
+  target="darwin"
+else
+  echo "Unsupport Operating System... only linux or macOS"
+  exit 1
+fi
+
+curl -sL "https://github.com/basd4g/mopm/releases/download/0.0.2/mopm-amd64-${target}.out" > /usr/bin/mopm
 chmod a+x /usr/bin/mopm
 
 echo "mopm is installed!"
